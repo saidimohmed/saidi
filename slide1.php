8c3802288f8090692320c56007025316b28dcd6b
saidimohmed <101113676+saidimohmed@users.noreply.github.com>
2022-03-24 22:21:40 +0100

    Update slide1.php

--- slide1.php
+++ slide1.php
@@ -1,73 +1,73 @@
 <div class="card-wrap">
     <div class="card">
         <div class="imgBox">
-            <img src="https://f.top4top.io/p_22742659i0.png">
+            <img src="https://b.top4top.io/p_2273zoc760.jpeg">
         </div>
         <div class="btn-claim" onclick="gass(this)">Claim</div>
     </div>
     <div class="card">
         <div class="imgBox">
-            <img src="https://e.top4top.io/p_2258fjhfq1.jpeg">
+            <img src="https://c.top4top.io/p_2273ew3hw1.jpeg">
         </div>
         <div class="btn-claim" onclick="gass(this)">Claim</div>
     </div>
     <div class="card">
         <div class="imgBox">
-            <img src="https://f.top4top.io/p_225802a5o2.jpeg">
+            <img src="https://d.top4top.io/p_2273vtp152.jpeg">
         </div>
         <div class="btn-claim" onclick="gass(this)">Claim</div>
     </div>
     <div class="card">
         <div class="imgBox">
-            <img src="https://h.top4top.io/p_2258qon7c4.jpeg">
+            <img src="https://e.top4top.io/p_22738x7cr3.jpeg">
         </div>
         <div class="btn-claim" onclick="gass(this)">Claim</div>
     </div>
     <div class="card">
         <div class="imgBox">
-            <img src="https://i.top4top.io/p_225877mwu5.jpeg">
+            <img src="https://f.top4top.io/p_2273xd5bw4.jpeg">
         </div>
         <div class="btn-claim" onclick="gass(this)">Claim</div>
     </div>
     <div class="card">
         <div class="imgBox">
-            <img src="https://j.top4top.io/p_2258yxz7i6.jpeg">
+            <img src="https://g.top4top.io/p_22730xfmm5.jpeg">
         </div>
         <div class="btn-claim" onclick="gass(this)">Claim</div>
     </div>
     <div class="card">
         <div class="imgBox">
-            <img src="https://k.top4top.io/p_2258tdjdw7.jpeg">
+            <img src="https://h.top4top.io/p_2273igju46.jpeg">
         </div>
         <div class="btn-claim" onclick="gass(this)">Claim</div>
     </div>
     <div class="card">
         <div class="imgBox">
-            <img src="https://l.top4top.io/p_2258btplg8.jpeg">
+            <img src="https://i.top4top.io/p_2273bek9s7.jpeg">
         </div>
         <div class="btn-claim" onclick="gass(this)">Claim</div>
     </div>
     <div class="card">
         <div class="imgBox">
-            <img src="https://a.top4top.io/p_2258bd7jl9.jpeg">
+            <img src="https://j.top4top.io/p_2273tz5qf8.jpeg>
         </div>
         <div class="btn-claim" onclick="gass(this)">Claim</div>
     </div>
     <div class="card">
         <div class="imgBox">
-            <img src="https://k.top4top.io/p_2258cf1750.jpeg">
+            <img src="https://k.top4top.io/p_22733y4hr9.jpeg">
         </div>
         <div class="btn-claim" onclick="gass(this)">Claim</div>
     </div>
     <div class="card">
         <div class="imgBox">
-            <img src="https://l.top4top.io/p_2258nih2w1.jpeg">
+            <img src="https://l.top4top.io/p_2273feyoe8.jpeg">
         </div>
         <div class="btn-claim" onclick="gass(this)">Claim</div>
     </div>
     <div class="card">
         <div class="imgBox">
-            <img src="https://a.top4top.io/p_2258bh7ct2.jpeg">
+            <img src="https://a.top4top.io/p_2273wz7l19.jpeg">
         </div>
         <div class="btn-claim" onclick="gass(this)">Claim</div>
     </div>
